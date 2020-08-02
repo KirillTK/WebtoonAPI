34bdf66bb54d48e44f9c3abd8136115ef30deecf
Kirill Tkachyov <k2.tkachyov@itransition.com>
2020-08-02 17:15:16 +0300

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins'
 import { uglify } from 'rollup-plugin-uglify';
-
-
 
 export default {
   input: 'lib/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs',
+    name: 'WebtoonParser',
   },
   plugins: [
     resolve({
-      extensions: ['.js'],
+      extensions: ['.js']
     }),
-    commonjs({
-      include: 'node_modules/**',
-    }),
+    builtins(),
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
+      runtimeHelpers: true,
     }),
+    commonjs(),
     uglify(),
   ]
 }
